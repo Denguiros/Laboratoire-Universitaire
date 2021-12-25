feb7372aabec6ef7d1723e51fbad3bb5442ae370
Soufiane Ben Yaala <soufianebenyaala1998@gmail.com>
2021-12-25 14:32:36 +0100

    .

--- src/app/component/member-show/member-show.component.ts
+++ src/app/component/member-show/member-show.component.ts
@@ -1,4 +1,21 @@
+import { Member } from './../../../models/member.model';
 import { Component, OnInit } from '@angular/core';
+
+var memberJSON = {
+  "id": "12",
+  "cin": "652365",
+  "nom": "Ben Yaala",
+  "prenom": "Soufiane",
+  "type": "TEACHER",
+  "cv": "string",
+  "email":"string@gmailcom",
+  "date":"2020-10",
+  "dateInscription": "2020-10",
+  "diplome": "PhD",
+  "encadreur":"string",
+  "grade": "PhD",
+  "etablissement": "ENIS"
+};
 
 @Component({
   selector: 'app-member-show',
@@ -6,10 +23,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-show.component.scss']
 })
 export class MemberShowComponent implements OnInit {
+  Member: Member = memberJSON;
 
-  constructor() { }
+  constructor() {
+    console.log(this.Member);
+  }
 
   ngOnInit(): void {
   }
+
 
 }
